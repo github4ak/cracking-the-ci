6115cac88a12acfc435b77973bba0475d84406ab
Abishek Krishnan <github4ak@gmail.com>
2019-09-27 16:19:42 -0600

    Add solution to question 1 of Chapter 5

---
+++ cracking-the-ci-project/src/chapter_5/ctci_5_1.java
@@ -0,0 +1,22 @@
+package chapter_5;
+
+public class ctci_5_1 {
+    
+    static int fitMN(int n,int  m,int j,int i){
+        int allOnes  = ~0;
+        int left = allOnes << (j+i);
+        int right = (1<<i)-1;
+        int mask = left | right;
+        int n_cleared = n & mask;
+        int m_shifted = m<<i;
+        return n_cleared | m_shifted;
+    }
+    
+    public static void main(String args[]){
+        int n = 1024;
+        int m = 19;
+        int start = 6;
+        int end = 2;
+        System.out.println("M fitted in N is "+fitMN(n,m,start,end));
+    }
+}
