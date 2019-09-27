8b9630ae4b6fb089fb2a6fe083b0e75a43637a03
Abishek Krishnan <github4ak@gmail.com>
2019-09-27 15:56:15 -0600

    Added Solution 1_1

---
+++ cracking-the-ci-project/src/cracking_the_ci/ctci_1_1.java
@@ -0,0 +1,30 @@
+package cracking_the_ci;
+
+public class ctci_1_1 {
+    
+    static boolean isUnique(String input){
+        
+        int strLength =  input.length();
+        
+        if(strLength > 128){
+            return false;
+        }
+        
+        boolean ascii[] = new boolean[128];
+        
+        for(int i = 0 ; i < strLength ; i++){
+            int val = input.charAt(i);
+            if(ascii[val]){
+                return false;
+            }
+            ascii[val] = true;
+        }
+        
+        return true;
+    }
+    
+    public static void main(String args[]){
+        String input = "Hello";
+        System.out.println("Has unique characters "+isUnique(input));
+    }
+}
