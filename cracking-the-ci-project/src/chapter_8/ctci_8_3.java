c8b3583ffc6ae16c56183a7319794a9b171d74d0
Abishek Krishnan <github4ak@gmail.com>
2019-10-05 16:51:51 -0600

    Add solution to Q3 of Chapter 8

---
+++ cracking-the-ci-project/src/chapter_8/ctci_8_3.java
@@ -0,0 +1,29 @@
+package chapter_8;
+
+//Part-2 : If not distinct, recurse through both the sides
+
+public class ctci_8_3 {
+	
+	static int findMI(int a[],int l, int r) {
+		while(l<=r) {
+			int mid = l + (r-l)/2;
+			
+			if(a[mid] == mid) {
+				return mid;
+			} else if(a[mid]>mid){
+				return findMI(a,l,mid-1);
+			} else {
+				return findMI(a,mid+1,r);
+			}
+		}
+		return -1;
+	}
+	
+	
+	public static void main(String args[]) {
+		int arr[] = {-40,-20,-1,1,2,3,5,7,9,12,13};
+		System.out.println("Magic Index present at "+findMI(arr,0,arr.length - 1));
+		
+	}
+
+}
