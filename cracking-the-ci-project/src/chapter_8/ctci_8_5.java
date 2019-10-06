a0a919f1a7b406b4f3f57d68d6ddfeacf0bd9aef
Abishek Krishnan <github4ak@gmail.com>
2019-10-05 20:07:46 -0600

    Add solution to Q5 of Chapter 8

---
+++ cracking-the-ci-project/src/chapter_8/ctci_8_5.java
@@ -0,0 +1,38 @@
+package chapter_8;
+
+public class ctci_8_5 {
+	
+	static int findmin(int x,int y) {
+		return x < y ? x : y;
+	}
+	
+	static int multiply(int a,int b) {
+		int min = findmin(a,b);
+		int max;
+		int ans = 0;
+		max = (min == a) ? b : a;
+
+		int mul2 = min/2;
+		int mulmod2 = min%2;
+		
+		while(mul2 > 0) {
+			ans+=max<<1;
+			mul2--;
+		}
+		
+		while(mulmod2 > 0) {
+			ans+=max;
+			mulmod2--;
+		}
+		
+		return ans;
+	}
+	
+	public static void main(String args[]) {
+		int a = 1000;
+		int b = 49;
+		
+		System.out.println(a+"*"+b+" is "+multiply(a,b));
+	}
+
+}
