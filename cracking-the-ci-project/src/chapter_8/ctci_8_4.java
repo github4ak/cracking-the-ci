6e16989772bcf0ba0c3a973c79b4012f1d32424e
Abishek Krishnan <github4ak@gmail.com>
2019-10-05 18:52:31 -0600

    Correct and simplify class Q4 Chapter 8

--- cracking-the-ci-project/src/chapter_8/ctci_8_4.java
+++ cracking-the-ci-project/src/chapter_8/ctci_8_4.java
@@ -11,17 +11,17 @@ public class ctci_8_4 {
 		int max = 1 << set.size();
 		
 		for(int j = 0 ; j <  max ; j++) {
-			ArrayList<Integer> subset = binaryToSet(j,set);
+			ArrayList<Integer> subset = binaryToSet(j);
 			allsubsets.add(subset);
 		}
 		
 		return allsubsets;
 	}
 	
-	static ArrayList<Integer> binaryToSet(int num,ArrayList<Integer> set) {
+	static ArrayList<Integer> binaryToSet(int num) {
 		ArrayList<Integer> subset = new ArrayList<Integer>();
 		int index = 0;
-		// L -> R binary number
+		// L -> R binary number : To remember
 		for(int k = num ; k >0 ; k>>=1) {
 			if((k & 1) == 1) {
 				subset.add(index);
@@ -32,7 +32,7 @@ public class ctci_8_4 {
 	}
 	
 	
-	public static void main(String args) {
+	public static void main(String[] args) {
 		ArrayList<Integer> set = new ArrayList<>();
 		for(int i=0; i <5;i++) {
 			set.add(i);
