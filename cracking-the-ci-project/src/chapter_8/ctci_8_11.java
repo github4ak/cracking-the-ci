b3006922bc3a974fe19b1312fbdc650d427f69b2
Abishek Krishnan <github4ak@gmail.com>
2019-10-22 15:26:00 -0600

    Modify solution to Q11 of Chapter 8

--- cracking-the-ci-project/src/chapter_8/ctci_8_11.java
+++ cracking-the-ci-project/src/chapter_8/ctci_8_11.java
@@ -1,16 +1,20 @@
 package chapter_8;
 
+import java.util.Arrays;
+
 public class ctci_8_11 {
 	
 	private static int ways(int total,int coins[],int n) {
 		int pos[] = new int[total+1];
 		
+		Arrays.fill(pos, 0);
+		
 		pos[0] = 1;
 		
 		for(int i=0; i<n;i++) {
-			for(int j = 0;j<=total;i++ ) {
-				if(j>=coins[i]) {
-					pos[i]+=pos[i-coins[j]];
+			for(int j = 0;j<=total;j++ ) {
+				if(j-coins[i]>=0) {
+					pos[j]+=pos[j-coins[i]];
 				}
 			}
 		}
@@ -18,10 +22,10 @@ public class ctci_8_11 {
 		return pos[total];
 	}
 	
-	public static void main() {
+	public static void main(String args[]) {
 		int n =  45;
 		int coins[] = {1,5,10,25};
 		
-		System.out.println("No. of ways to sum up the coins is"+ways(n,coins,coins.length));
+		System.out.println("No. of ways to sum up the coins is "+ways(n,coins,coins.length));
 	}
 }
