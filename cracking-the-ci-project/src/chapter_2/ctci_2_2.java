246305e1d7fdb86c87966617e3683cd8340c2f5d
Abishek Krishnan <github4ak@gmail.com>
2019-10-24 18:04:43 -0600

    Add solution to Q2 of Chapter 2

---
+++ cracking-the-ci-project/src/chapter_2/ctci_2_2.java
@@ -0,0 +1,36 @@
+package chapter_2;
+
+/* Good feeling to implement tortoise and head algorithm */
+
+public class ctci_2_2 {
+	public static void main(String args[]) {
+		Node head = new Node(1);
+		head.next = new Node(2);
+		head.next.next = new Node(3);
+		head.next.next.next = new Node(4);
+		head.next.next.next.next = new Node(5);
+		
+		int k = 3;
+		
+		findkthToLastElement(head,k);
+	}
+	
+	private static void findkthToLastElement(Node head,int k) {
+		Node p1 = head;
+		Node p2 = head;
+		
+		int i = 0;
+		
+		while(i<k) {
+			p1 = p1.next;
+			i++;
+		}
+		
+		while(p1!=null) {
+			p1 = p1.next;
+			p2 = p2.next;
+		}
+		
+		System.out.println("The "+k+"-kth element is "+p2.data);
+	}
+}
