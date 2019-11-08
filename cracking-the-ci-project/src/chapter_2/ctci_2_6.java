e72fb932e107caa2aa3fb7a0995d01c3ccca34fd
Abishek Krishnan <github4ak@gmail.com>
2019-11-07 17:47:56 -0700

    Add solution to Q6 of Chapter 2

---
+++ cracking-the-ci-project/src/chapter_2/ctci_2_6.java
@@ -0,0 +1,43 @@
+package chapter_2;
+
+import java.util.Stack;
+
+public class ctci_2_6 {
+	public static void main(String args[]) {
+		Node head = new Node(1);
+		head.next = new Node(6);
+		head.next.next = new Node(2);
+		head.next.next.next = new Node(6);
+		head.next.next.next.next = new Node(1);
+		head.next.next.next.next.next = new Node(2);
+		
+		System.out.println("Is palindrome ? "+isPalindrome(head));
+	}
+	
+	private static boolean isPalindrome(Node head) {
+		Node slow = head;
+		Node fast = head;
+		
+		Stack<Integer> fList = new Stack<>();
+		
+		while(fast != null && fast.next != null) {
+			fList.push(slow.data);
+			slow =  slow.next;
+			fast = 	fast.next.next;
+		}
+		
+		if(fast != null) {
+			slow = slow.next;
+		}
+		
+		while(slow != null) {
+			int stackData = fList.pop();
+			if(stackData != slow.data) {
+				return false;
+			}
+			slow = slow.next;
+		}
+		
+		return true;
+	}
+}
