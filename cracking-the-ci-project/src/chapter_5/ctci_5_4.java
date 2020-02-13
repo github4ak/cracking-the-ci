a53106b1e2581fda36e48251b1ddda3e91140ed3
Abishek Krishnan <github4ak@gmail.com>
2020-02-12 22:07:21 -0700

    Add solution to Q4 of Chapter-5

---
+++ cracking-the-ci-project/src/chapter_5/ctci_5_4.java
@@ -0,0 +1,39 @@
+package chapter_5;
+
+public class ctci_5_4 {
+
+	public static void main(String args[]) {
+		int a = 1775;
+		System.out.println("Next largest with same bits "+findNextLarge(a));
+		System.out.println("Next smallest with same bits "+findNextSmall(a));
+	}
+
+	private static int findNextSmall(int ip) {
+		int bitcount = Integer.bitCount(ip);
+		ip = ip - 1;
+		boolean found = false;
+		while(!found && ip > 0) {
+			if(Integer.bitCount(ip) == bitcount) {
+				found = true;
+			} else {
+			ip = ip - 1;
+			}
+		}
+		return ip;
+	}
+
+	private static int findNextLarge(int ip) {
+		int bitcount = Integer.bitCount(ip);
+		ip =  ip + 1;
+		boolean found = false;
+		while(!found) {
+			if(Integer.bitCount(ip) == bitcount) {
+				found = true;
+			} else {
+			ip = ip + 1;
+			}
+		}
+		return ip;
+	}
+
+}
