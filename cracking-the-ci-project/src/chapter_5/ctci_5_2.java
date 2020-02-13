c28f047736b80afdf17690fec7d8769a3a397262
Abishek Krishnan <github4ak@gmail.com>
2020-02-12 20:50:46 -0700

    Add solution to Q2 of Chapter-5

---
+++ cracking-the-ci-project/src/chapter_5/ctci_5_2.java
@@ -0,0 +1,39 @@
+package chapter_5;
+
+import java.util.Arrays;
+
+public class ctci_5_2 {
+	public static void main(String args[]) {
+		double realno = 0.5625;
+		int binarr[] = new int[32];
+		int filled = 0;
+		
+		while(realno != 1 && filled < 31) {
+			if(realno < 1) {
+				binarr[filled] = 0;
+			}
+			else {
+				binarr[filled] = 1;
+				realno = realno - 1;
+			}
+			realno = realno * 2;
+			filled++;
+		}
+		
+		if(realno ==1 && filled < 31) {
+			binarr[filled] = 1;
+			System.out.println(Arrays.toString(binarr));
+			printbin(binarr);
+		} else {
+			System.out.println("ERROR");
+		}
+	}
+
+	private static void printbin(int[] binarr) {
+		System.out.print("0.");
+		for(int i : binarr) {
+			System.out.print(i);
+		}	
+	}
+
+}
